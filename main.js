efe309e7d8c11a7c2f452f1d13df4b17b6fbf103
Nataliya Rohochaya <lc4821577@gmail.com>
2020-11-06 18:16:11 +0200

    features: kids mobiles

---
+++ main.js
@@ -0,0 +1,26 @@
+
+const   burger = document.getElementsByClassName("burger")[0];
+burger.addEventListener("click", function () {
+    this.classList.toggle("active")
+});
+
+const buttonGrid = document.getElementsByClassName("grid-js")[0];
+const buttonList = document.getElementsByClassName("list-js")[0];
+const list = document.getElementsByClassName("games-list");
+
+    buttonGrid.addEventListener("click", function () {
+        buttonList.classList.remove("active");
+        this.classList.add('active');
+        for (let i = 0; i < list.length; i++) {
+            list[i].classList.add("grid")
+        }
+    });
+buttonList.addEventListener("click", function () {
+    buttonGrid.classList.remove("active");
+    this.classList.add('active');
+    for (let i = 0; i < list.length; i++) {
+        list[i].classList.remove("grid");
+    }
+});
+
+
