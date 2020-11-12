6a2c480822660e573388b89b48c655a6d4d9d979
Nataliya Rohochaya <lc4821577@gmail.com>
2020-11-12 16:26:24 +0200

    fix: header

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const burger = document.getElementsByClassName("burger")[0];
+const menu = document.getElementsByClassName("menu-content")[0];
 burger.addEventListener("click", function () {
-    this.classList.toggle("active")
+    this.classList.toggle("active");
+    menu.classList.toggle("mobile-menu")
 });
 
 const buttonGrid = document.getElementsByClassName("grid-js")[0];
@@ -11,15 +13,19 @@ buttonGrid.addEventListener("click", function () {
     buttonList.classList.remove("active");
     this.classList.add('active');
     for (let i = 0; i < list.length; i++) {
-        list[i].classList.add("grid")
+        list[i].classList.remove("list");
     }
 });
 buttonList.addEventListener("click", function () {
     buttonGrid.classList.remove("active");
     this.classList.add('active');
     for (let i = 0; i < list.length; i++) {
-        list[i].classList.remove("grid");
+        list[i].classList.add("list")
     }
 });
 
+const sidebarMenu = document.getElementsByClassName("submenu")[0];
+sidebarMenu.addEventListener("click", function () {
+    this.classList.toggle("show");
+});
 
