7fdb485577dbb3191487f2de51d72257a9e5e671
Nataliya Rohochaya <lc4821577@gmail.com>
2020-11-13 20:03:21 +0200

    fix: animation

--- main.js
+++ main.js
@@ -9,20 +9,25 @@ const buttonGrid = document.getElementsByClassName("grid-js")[0];
 const buttonList = document.getElementsByClassName("list-js")[0];
 const list = document.getElementsByClassName("games-list");
 
-buttonGrid.addEventListener("click", function () {
-    buttonList.classList.remove("active");
-    this.classList.add('active');
-    for (let i = 0; i < list.length; i++) {
-        list[i].classList.remove("list");
-    }
-});
-buttonList.addEventListener("click", function () {
-    buttonGrid.classList.remove("active");
-    this.classList.add('active');
-    for (let i = 0; i < list.length; i++) {
-        list[i].classList.add("list")
-    }
-});
+if (buttonGrid) {
+    buttonGrid.addEventListener("click", function () {
+        buttonList.classList.remove("active");
+        this.classList.add('active');
+        for (let i = 0; i < list.length; i++) {
+            list[i].classList.remove("list");
+        }
+    });
+
+    buttonList.addEventListener("click", function () {
+        buttonGrid.classList.remove("active");
+        this.classList.add('active');
+        for (let i = 0; i < list.length; i++) {
+            list[i].classList.add("list")
+        }
+    });
+}
+
+
 
 const sidebarMenu = document.getElementsByClassName("submenu");
 for (let i = 0; i < sidebarMenu.length; i++) {
@@ -40,5 +45,110 @@ if (window.innerWidth < 992) {
         });
     }
 }
+
+
+
+
+var check_status = false;
+
+$(".heart .like-cnt").click(function(){
+    var t1 = new TimelineLite();
+    var t2 = new TimelineLite();
+    if(!check_status){
+        t1.set(this, {scale:0});
+        t1.set(this, {scale: 0});
+        t1.to(this, 0.6, {scale:1, opacity: 1,ease: Expo.easeOut});
+        t2.to(this, 0.65, {scale: 1, ease: Elastic.easeOut.config(1, 0.3)}, '+=0.2');
+        check_status=true;
+       var burst = new mojs.Burst({
+            parent:$( this ).parent(),
+            radius:   { 10: 30 },
+            count: 15,
+            angle:{0:30},
+            children: {
+                delay: 250,
+                duration: 700,
+                radius:{10: 0},
+                opacity:   1,
+                easing: 		mojs.easing.bezier(.08,.69,.39,.97)
+            }
+        });
+        burst.replay()
+    }
+    else{
+        t1.to(this, 1, {scale:1})
+            .to(this, 1, {scale:1, opacity: 0.4, ease: Power4.easeOut});
+        t1.timeScale(7);
+        check_status=false;
+    }
+
+});
+
+$(".like-js .like-cnt").click(function(){
+    var t1 = new TimelineLite();
+    var t2 = new TimelineLite();
+    if(!check_status){
+        t1.set(this, {scale:0});
+        t1.set(this, {scale: 0});
+        t1.to(this, 0.6, {scale:1, background: 'rgba(0, 22, 255, 1)',ease: Expo.easeOut});
+        t2.to(this, 0.65, {scale: 1, ease: Elastic.easeOut.config(1, 0.3)}, '+=0.2');
+        check_status=true;
+        var burst = new mojs.Burst({
+            parent:$( this ).parent(),
+            radius:   { 10: 30 },
+            count: 15,
+            angle:{0:30},
+            children: {
+                delay: 250,
+                duration: 700,
+                radius:{10: 0},
+                fill:   [ 'rgba(0, 22, 255, 1)' ],
+                easing: 		mojs.easing.bezier(.08,.69,.39,.97)
+            }
+        });
+        burst.replay()
+    }
+    else{
+        t1.to(this, 1, {scale:1})
+            .to(this, 1, {scale:1, background: '#23d2e2', ease: Power4.easeOut});
+        t1.timeScale(7);
+        check_status=false;
+    }
+
+});
+
+$(".dislike .like-cnt").click(function(){
+    var t1 = new TimelineLite();
+    var t2 = new TimelineLite();
+    if(!check_status){
+        t1.set(this, {scale:0});
+        t1.set(this, {scale: 0});
+        t1.to(this, 0.6, {scale:1, background: 'rgba(0, 22, 255, 1)',ease: Expo.easeOut});
+        t2.to(this, 0.65, {scale: 1, ease: Elastic.easeOut.config(1, 0.3)}, '+=0.2');
+        check_status=true;
+        var burst = new mojs.Burst({
+            parent:$( this ).parent(),
+            radius:   { 10: 30 },
+            count: 15,
+            angle:{0:30},
+            children: {
+                delay: 250,
+                duration: 700,
+                radius:{10: 0},
+                fill:   [ 'rgba(0, 22, 255, 1)' ],
+                easing: 		mojs.easing.bezier(.08,.69,.39,.97)
+            }
+        });
+        burst.replay()
+    }
+    else{
+        t1.to(this, 1, {scale:1})
+            .to(this, 1, {scale:1, background: '#23d2e2', ease: Power4.easeOut});
+        t1.timeScale(7);
+        check_status=false;
+    }
+
+});
+
 
 
